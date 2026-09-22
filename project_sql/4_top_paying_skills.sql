941092625659ac2b417e9761cab74c4beb3945df
Heitor Vieira <heitorvieira1220@gmail.com>
2026-09-22 14:59:18 -0300

    optimal_skills query

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -16,6 +16,7 @@ INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst'
     AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True 
 GROUP BY skills
 ORDER BY avg_salary DESC
 LIMIT 20;
--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -5,3 +5,44 @@ Answer: What are the most optimal skills to learn (aka it’s in high demand and
 - Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), 
     offering strategic insights for career development in data analysis
 */
+
+WITH top_demanded_skills AS (
+    SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS number_of_jobs_requiring_this_skill
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst' 
+        AND job_work_from_home = True 
+        AND salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+), top_paying_skills AS (
+    SELECT 
+        skills_job_dim.skill_id,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True 
+    GROUP BY
+        skills_job_dim.skill_id
+)
+
+SELECT 
+    top_demanded_skills.skill_id,
+    top_demanded_skills.skills,
+    avg_salary,
+    number_of_jobs_requiring_this_skill
+FROM top_demanded_skills
+INNER JOIN top_paying_skills ON top_demanded_skills.skill_id = top_paying_skills.skill_id
+ORDER BY 
+    number_of_jobs_requiring_this_skill DESC,
+    avg_salary DESC
+LIMIT 25
